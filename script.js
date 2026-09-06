82e566b209d925a217b1d692c39973ceac87e12f
Gabriell <gabriellsantos860@gmail.com>
2026-09-06 17:49:22 -0300

    arrow function, também apareceu em map()

--- script.js
+++ script.js
@@ -39,3 +39,12 @@ const teste = {
 };
 const{primeiro, segundo} = teste;
 console.log(primeiro, segundo);
+
+function somar(a, b) {
+    return a + b;
+}
+const somar = (a, b) => {
+    return a + b;
+};
+
+const somar = (a, b) => a + b;
