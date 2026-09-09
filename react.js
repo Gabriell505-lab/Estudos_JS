4ecb92c1e318b20a87bb28ccd5c28e54113fcf43
Gabriell <gabriellsantos860@gmail.com>
2026-09-09 20:46:44 -0300

    Props dados passados a outro componente

--- react.js
+++ react.js
@@ -10,8 +10,15 @@ export default App;
 
 function Site() {
     const nome = "Gabriell";
+    <div><Usuario nome = "Gabs" />;</div>
 
     return (
         <h1>Óla, {nome}!</h1>
     );
 }
+
+function Usuario({ nome }) {
+    return <h2>Olá, {nome}</h2>;
+}
+
+console.log(Usuario);
